a8748d7f736b5ca7484b948510ff505171c4270f
lightsing <light.tsing@gmail.com>
2020-02-10 11:52:14 +0800

    generates font file with human-readable filename

--- src/utils/fetch.py
+++ src/utils/fetch.py
@@ -11,14 +11,23 @@ def main():
     args = parser.parse_args()
 
     with open(args.css) as reader:
-        url_re = re.compile(r"//([a-zA-Z0-9\-./_]+\.woff)")
-        fname_re = re.compile(r"([a-zA-Z0-9]+\.woff)")
-        urls = set(url_re.findall(reader.read()))
+        # fname_re = re.compile(r"([a-zA-Z0-9]+\.woff)")
+        buffer = reader.read().split("@font-face{")
+        buffer = list(filter(None, buffer))
+    font_name_re = re.compile(r"font-family:\s+\"([\w-]+)\"")
+    url_re = re.compile(r"//([a-zA-Z0-9\-./_]+\.woff)")
+    font_names = []
+    urls = []
+    for line in buffer:
+        font_names.append(font_name_re.findall(line)[0])
+        urls.append(url_re.findall(line)[0])
 
-    fnames = [fname_re.findall(url)[0] for url in urls]
-    for fname, url in zip(fnames, urls):
+    print(font_names, urls)
+    assert len(font_names) == len(urls)
+    # fnames = [fname_re.findall(url)[0] for url in urls]
+    for fname, url in zip(font_names, urls):
         resp = requests.get('https://{}'.format(url), allow_redirects=True)
-        with open(fname, 'wb') as writer:
+        with open('{}.woff'.format(fname), 'wb') as writer:
             writer.write(resp.content)
 
 
