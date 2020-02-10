1bb938ccfa20cd7bc3a1e83c31084686d62ad40a
lightsing <light.tsing@gmail.com>
2020-02-10 12:30:21 +0800

    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='sfontmapper',
+      version='0.1.0',
+      description='Another Anti-Anti-Spider font mapping tools',
+      author='Akase Cho',
+      author_email='akase@moumoux.com',
+      url='http://git.dev.moumoux.com/ergedd/sfontmapper',
+      packages=['sfontmapper'],
+      )
